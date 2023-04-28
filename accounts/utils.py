b920253fd54239a0869cc0d06f6ad8bddf7a330f
Ritika Jain <ritikajain1505@gmail.com>
2023-04-29 05:03:32 +0530

    forgot password feature

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,11 +1,11 @@
-"""from django.contrib import messages
+#from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage, message
-from django.conf import settings"""
+from django.conf import settings
 
 def detectUser(user):
     if user.role == 1:
@@ -19,7 +19,7 @@ def detectUser(user):
         return redirectUrl
 
     
-"""def send_verification_email(request, user, mail_subject, email_template):
+def send_verification_email(request, user, mail_subject, email_template):
     from_email = settings.DEFAULT_FROM_EMAIL
     current_site = get_current_site(request)
     message = render_to_string(email_template, {
@@ -30,11 +30,10 @@ def detectUser(user):
     })
     to_email = user.email
     mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
-    mail.content_subtype = "html"
     mail.send()
 
 
-def send_notification(mail_subject, mail_template, context):
+"""def send_notification(mail_subject, mail_template, context):
     from_email = settings.DEFAULT_FROM_EMAIL
     message = render_to_string(mail_template, context)
     if(isinstance(context['to_email'], str)):
